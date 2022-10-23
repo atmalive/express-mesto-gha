340b7ab72e355e61d3bba65fb0c7a56c914816f7
Popov Mikhail <atmalive@gmail.com>
2022-10-24 01:01:12 +0200

    fix: add email validation

--- app.js
+++ app.js
@@ -31,7 +31,8 @@ app.post('/signin', celebrate({
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().min(2).max(30)
+      .email(),
     password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
