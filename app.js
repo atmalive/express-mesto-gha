61acf3ed5506841708c34e77d4babf0b7da18808
Popov Mikhail <atmalive@gmail.com>
2022-10-24 00:20:57 +0200

    fix: pr14 joi sing in-up

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const { celebrate, Joi } = require('celebrate');
 const routerUsers = require('./routes/users'); // импортируем роутер
 const routerCards = require('./routes/cards');
 const { ERRORS } = require('./utils/errors');
 const { login, postUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const { handleErrors } = require('./middlewares/errors');
+const { regex } = require('./utils/regex');
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 
@@ -21,8 +23,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.post('/signin', login);
-app.post('/signup', postUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({ email: Joi.string().required().min(2).email(), password: Joi.string().required() }),
+}), login);
+
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().min(2).max(30),
+    password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    link: Joi.string().pattern(new RegExp(regex)),
+  }),
+}), postUser);
 
 app.use(auth);
 
